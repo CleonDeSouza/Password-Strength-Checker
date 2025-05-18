4477dad499604c79922a6525d5b4ce8fbe4d5439
Cleon Souza <154812561+CleonDeSouza@users.noreply.github.com>
2025-05-18 19:31:53 +0100

    Update psc.py

--- psc.py
+++ psc.py
@@ -9,9 +9,12 @@ def check_pass(x):
      if rex.search(r'[a-z]', x): strength += 1     
      if rex.search(r'[0-9]', x): strength += 1
      if rex.search(r'[^A-Za-z0-9]', x): strength += 1
-     
+     if strength == -1: strength += 1
+
      strength = min(strength, 4)
      return cl_strength[strength]
+
+
 
 """
 while True:
